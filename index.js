f2baf0233d228d3238a08e5f323be691ede83b90
ArcherHuang <mmosconii@gmail.com>
2025-04-10 08:31:14 +0800

    Update

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // index.js
 const express = require('express');
 const app = express();
-const port = 3000;
 
-// GET API 路由
-// http://localhost:3000/api/hello
+// 建議監聽 127.0.0.1 而不是所有接口
+const port = process.env.PORT || 3000; // IISNODE 會提供 PORT
+const host = '127.0.0.1';
+
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello, world!' });
 });
 
-// 啟動伺服器
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(port, host, () => {
+  console.log(`Server is running at http://${host}:${port}`);
 });
